440143aa0bde6dbb186614c2c7b3a656f558a6e4
PrinnnX <prin@cryptovation.co>
2018-09-26 16:12:18 +0700

    fix ForexDispatcher

--- app/Jobs/ForexDispatcher.php
+++ app/Jobs/ForexDispatcher.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\Tickers\TickerRedis;
 
-class OrderDispatcher implements ShouldQueue
+class ForexDispatcher implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
